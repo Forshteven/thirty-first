40606790304e1762c9cc5d86f7b3cf79da20e658
Forshteven <geocinds@mail.ru>
2024-10-07 15:23:38 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+
+def custom_write(file_name, strings):
+    file = open(file_name, 'a')
+    pprint(file.append(strings))
+    file.close()
+
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
