8582274ed10fc7801df52fec268165068f0b0ad6
Forshteven <geocinds@mail.ru>
2024-10-09 12:36:38 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -1,10 +1,17 @@
-from pprint import pprint
-
 
 def custom_write(file_name, strings):
-    file = open(file_name, 'a')
-    pprint(file.append(strings))
+    file = open(file_name, 'w', encoding='utf-8')
+    string_positions = {}
+    str_num = 0
+    str_start_byte = file.seek(0)
+    for string_ in strings:
+        file.write(string_+'\n')
+        str_num += 1
+        key = (str_num, str_start_byte)
+        string_positions[key] = string_
+        str_start_byte = file.tell()
     file.close()
+    return string_positions
 
 
 
@@ -17,4 +24,4 @@ info = [
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
+    print(elem)
